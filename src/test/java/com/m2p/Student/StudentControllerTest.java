85ddee3fd19f766792824389f6bada47104bb9ec
Rahul-Chhabra-27 <chhabrarahul2707@gmail.com>
2023-06-23 19:18:20 +0530

    Controller Layer Test added.

--- src/test/java/com/m2p/Student/StudentControllerTest.java
+++ src/test/java/com/m2p/Student/StudentControllerTest.java
@@ -1,21 +1,73 @@
 package com.m2p.Student;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = StudentController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
 
 class StudentControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private StudentService studentService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
-    void shouldGetAllStudents() {
+    void shouldGetAllStudents() throws Exception {
 
+        when(studentService.getStudents())
+                .thenReturn(Arrays
+                        .asList(new Student("Anni",20),new Student("Rahul",1)));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/students"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(Arrays
+                        .asList(new Student("Anni",20),new Student("Rahul",1)))));
     }
 
     @Test
-    void getStudentById() {
+    void getStudentById() throws Exception {
+        Student student = new Student("Rahul",1);
+
+        when(studentService.getStudent(student.getId())).thenReturn(Optional.of(student));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/student/{id}",student.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(student)));
     }
 
     @Test
-    void saveStudent() {
+    void saveStudent() throws Exception {
+        Student student = new Student("Rahul",1);
+
+        when(studentService.saveStudent(student)).thenReturn(student);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/student")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(student)))
+                .andExpect(status().isCreated())
+                .andExpect(content().json(objectMapper.writeValueAsString(student)));
     }
 }
